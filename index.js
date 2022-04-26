1cfac7f8f4230563d3d74ceb271fa668325c0fa7
Saracode13 <104016036+Saracode13@users.noreply.github.com>
2022-04-26 18:15:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+let edad= 18;
+
+if (edad < 14) {
+    console.log ("Lo siento, no puedes pasar")
+} 
+else if (edad>=14 && edad <=17){
+    console.log ("puedes entrar acompañado de un adulto")
+}
+
+let event= "Beyoncé"
+let ticket=true;
+
+if (ticket=true){
+    console.log("Puedes pasar")
+} else{
+    console.log("No puedes pasar, no tienes ticket ")
+}
+
+if (edad >18 && ticket && event==="Beyoncé"){
+    console.log("Puedes pasar")
+} else if (edad<14 && ticket===yes && name===Beyoncé){
+    console.log ("No puedes pasar")
+} else if (edad>=14 && edad <=17 && ticket===Yes){
+    console.log ("puedes entrar acompañado de un adulto")
+} 
+
+   
+
+
+
